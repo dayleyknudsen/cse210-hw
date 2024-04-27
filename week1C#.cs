564db9b062b7129ca7aa83a2d7c8baad415c451b
dayleyknudsen <eaglehazard2319@gmail.com>
2024-04-27 17:55:48 -0500

    hw

---
+++ week1C#.cs
@@ -0,0 +1,13 @@
+Console.WriteLine("Hello World!");
+
+Console.Write("What is your favorite color? ");
+string color = Console.ReadLine();
+
+if (x > y)
+{
+  Console.WriteLine("greater");
+}
+
+string school = "BYU-Idaho";
+Console.WriteLine($"I am studying at {school}.");
+
