af7ad1585661fed0e6144792188409bdca7c07a0
dayleyknudsen <eaglehazard2319@gmail.com>
2024-04-30 14:08:35 -0500

    New

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+          Job job1 = new Job();
+        job1._jobTitle = "Software Development";
+        job1._company = "Pathway";
+        job1._startYear = 2020;
+        job1._endYear = 2024;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Apprentice";
+        job2._company = "Tree";
+        job2._startYear = 2012;
+        job2._endYear = 2020;
+
+        Resume myResume = new Resume();
+        myResume._name = "Dayley Knudsen";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
